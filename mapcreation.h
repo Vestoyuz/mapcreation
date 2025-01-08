52bbb8407dd8b6c301284847ab24de0a08715a66
Vestoyuz <Vestoyuz@gmail.com>
2025-01-08 17:22:48 +0100

    Update mapcreation.h

--- mapcreation.h
+++ mapcreation.h
@@ -7,17 +7,17 @@ using std::endl;
 
 class Map {
 public:
-   char map[25][75];
+   char map[25][75]; // Change them to match your preferred height and width
    int height = sizeof(map) / sizeof(map[0]);
    int width = sizeof(map[0]) / sizeof(map[0][0]);
 
-   void FrameSpace(int spaces){
+   void FrameSpace(int spaces){ // Frame Spaces if you wanna do some console games
       for(int i = 0; i <= spaces; i++){
          cout << endl;
       }
    }
 
-   void FillTile(char symbol){
+   void FillTile(char symbol){ // Fill the map with your desired symbol
    for(int i = 0; i < height; i++){
       for(int ii = 0; ii < width; ii++){
          map[i][ii] = symbol;
@@ -25,7 +25,7 @@ public:
       }
    }
 
-   void PrintTile(){
+   void PrintTile(){ // Print the tile
       for(int i = 0; i < height; i++){
       cout << endl;
          for(int ii = 0; ii < width; ii++){
